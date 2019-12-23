457e4f33ae2b4e937b1236a626796de68a687b60
Sergey Bayramov <bayramov.sergi@gmail.com>
2019-12-23 03:27:06 +0300

    moved to gradle

--- src/main/java/mpi/Application.java
+++ src/main/java/mpi/Application.java
@@ -3,7 +3,9 @@ package mpi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
